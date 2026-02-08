534c5653cd19f4c8c61c577ad19a6d1ad0c5feea
mbredets <mr.mighdadjaba@gmail.com>
2026-02-08 19:41:37 +0800

    fix typo

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,7 +4,7 @@ import songs from '../services/songs/routes/index.js';
 import users from '../services/users/routes/index.js';
 import authentications from '../services/authentications/routes/index.js';
 import playlists from '../services/playlists/routes/index.js';
-import collaorations from '../services/collaborations/routes/index.js';
+import collaborations from '../services/collaborations/routes/index.js';
 
 const router = express.Router();
 
@@ -13,6 +13,6 @@ router.use('/songs', songs);
 router.use('/users', users);
 router.use('/authentications', authentications);
 router.use('/playlists', playlists);
-router.use('/collaborations', collaorations);
+router.use('/collaborations', collaborations);
 
 export default router;
