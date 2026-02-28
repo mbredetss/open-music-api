7e17f3f081603c5e46b4b6b23c55001ce7a8b754
mbredets <mr.mighdadjaba@gmail.com>
2026-02-28 13:46:51 +0800

    make album input validation stricter

--- src/services/albums/validator/schema.js
+++ src/services/albums/validator/schema.js
@@ -1,6 +1,6 @@
 import Joi from 'joi';
 
 export const albumPayloadSchema = Joi.object({
-  name: Joi.string().required().max(500),
-  year: Joi.number().required()
+  name: Joi.string().min(1).max(100).required(),
+  year: Joi.number().integer().min(1900).max(new Date().getFullYear()).required(), 
 });
