9b0dd4022f6236f5c59347023e7201f2cfc8d3c0
mbredets <mr.mighdadjaba@gmail.com>
2026-02-28 13:04:04 +0800

    change validate function to not use data from req.query

--- src/middlewares/validate.js
+++ src/middlewares/validate.js
@@ -1,5 +1,5 @@
 const validate = (schema) => (req, res, next) => {
-  const { error, value } = schema.validate(req.body ?? req.query, {
+  const { error, value } = schema.validate(req.body, {
     abortEarly: false,
     allowUnknown: false,
     stripUnknown: true
