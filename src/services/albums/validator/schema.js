d1b779b3ce2c94bb3a3e3c86b22afd877c38c66f
mbredets <mr.mighdadjaba@gmail.com>
2026-02-28 14:33:43 +0800

    fix lint error

--- src/services/albums/validator/schema.js
+++ src/services/albums/validator/schema.js
@@ -2,5 +2,5 @@ import Joi from 'joi';
 
 export const albumPayloadSchema = Joi.object({
   name: Joi.string().min(1).max(100).required(),
-  year: Joi.number().integer().min(1900).max(new Date().getFullYear()).required(), 
+  year: Joi.number().integer().min(1900).max(new Date().getFullYear()).required(),
 });
