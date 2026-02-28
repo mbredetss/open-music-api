310d8fc39ceef9d095a78de4e13968853e5f8ca0
mbredets <mr.mighdadjaba@gmail.com>
2026-02-28 14:33:36 +0800

    update eslint config to ignore camel case error in migration module

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,10 @@
 import globals from 'globals';
 import pluginJs from '@eslint/js';
 import daStyle from 'eslint-config-dicodingacademy';
- 
 
 export default [
   daStyle,
+  { files: ['migrations/**/*.js'], rules: { camelcase: 'off' } },
   { files: ['**/*.js'], languageOptions: { sourceType: 'module' } },
   { languageOptions: { globals: globals.node } },
   pluginJs.configs.recommended,
