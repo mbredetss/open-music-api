896f71c9a891107564530e04345e4ab338da8bf5
mbredets <mr.mighdadjaba@gmail.com>
2026-02-28 16:49:05 +0800

    make code readable

--- src/middlewares/verify-author.js
+++ src/middlewares/verify-author.js
@@ -3,18 +3,15 @@ import { response } from '../utils/index.js';
 
 export const verifyPlaylistAuthor = async (req, res, next) => {
   const playlistId = req.params.id ?? req.body.playlistId;
+  
   const result = await playlistRepositories.getPlaylistAuthor(playlistId);
 
-  if (!result) {
-    return response(res, 404, 'Playlist tidak ditemukan!');
-  }
+  if (!result) return response(res, 404, 'Playlist tidak ditemukan!');
 
   const playlistAuthor = result.userId;
   const userId = req.user.id;
 
-  if (playlistAuthor === userId) {
-    return next();
-  }
+  if (playlistAuthor === userId) return next();
 
   return response(res, 403, 'Unauthorized', null);
 };
